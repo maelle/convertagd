17fdc325bad65a59f3d55a14822d88eefca4f043
masalmon <maelle.salmon@yahoo.se>
2016-01-26 18:48:39 +0100

    adds example to README

--- R/read_agd.R
+++ R/read_agd.R
@@ -17,118 +17,34 @@
 #' testRes[[1]]
 #' testRes[[2]]
 read_agd <- function(file, tz = "GMT"){
+  # Connect to database and fetch the settings table
   con <- DBI::dbConnect(RSQLite::SQLite(), file)
   ad.set <- DBI::dbReadTable(con,"settings")
   ad.set <<- ad.set
-  origen<- as.POSIXct(as.character(as.POSIXlt((as.numeric(
-    ad.set[grep("startdatetime",ad.set[,2]),3])/1e7),
-    origin="0001-01-01 00:00:00", tz ="GMT")),tz=tz)
-  longitud <- as.numeric(ad.set[grep("epochcount",ad.set[,2]),3])
+  # find start date
+  rawStart <- ad.set$settingValue[ad.set$settingName=="startdatetime"]
+  # first convert to POSIXlt
+  origen <- as.POSIXlt((as.numeric(rawStart)/1e7),
+                       origin="0001-01-01 00:00:00", tz ="GMT")
+  # then change to write timezone
+  origen <- as.POSIXct(as.character(origen),tz=tz)
 
+  # find length
+  longitud <- ad.set$settingValue[ad.set$settingName=="epochcount"]
+  longitud <- as.numeric(longitud)
+  # now fetch the measurements table
   base <- DBI::dbReadTable(con, "data")
-  base$date <-origen+seq(from=0,by=10, length.out=longitud)
-  base <- base[c("date",names(base)[-c(1,dim(base)[2])])]
-  base2 <-pa.acti2(base,x)
+  base <- dplyr::tbl_df(base)
+  base <- dplyr::mutate(base,
+                        date = origen + seq(from=0,by=10,
+                        length.out=longitud))
+  base <- dplyr::select(base, date,
+                        everything(),
+                        -dataTimestamp)
 
   ad.set <- dplyr::tbl_df(ad.set)
-  res <- list(
-    settings=ad.set,
-    raw.data=base2)
+  res <- list(settings=ad.set,
+              raw.data=base)
   return(res)
 }
-
-
-
-pa.acti2 <-function(ace,x){
-
-
-  ace$cont<-1:dim(ace)[1]
-
-  ace$axis1.1  <- c(ace[2:dim(ace)[1],c("axis1")],NA)
-  ace$axis1.2  <- c(ace[3:dim(ace)[1],c("axis1")],rep(NA,2))
-  ace$axis1.3  <- c(ace[4:dim(ace)[1],c("axis1")],rep(NA,3))
-  ace$axis1.4  <- c(ace[5:dim(ace)[1],c("axis1")],rep(NA,4))
-  ace$axis1.5  <- c(ace[6:dim(ace)[1],c("axis1")],rep(NA,5))
-  ace$axis1._1 <- c(NA,ace[1:((dim(ace)[1])-1),c("axis1")])
-  ace$axis1._2 <- c(rep(NA,2),ace[1:((dim(ace)[1])-2),c("axis1")])
-  ace$axis1._3 <- c(rep(NA,3),ace[1:((dim(ace)[1])-3),c("axis1")])
-  ace$axis1._4 <- c(rep(NA,4),ace[1:((dim(ace)[1])-4),c("axis1")])
-  ace$axis1._5 <- c(rep(NA,5),ace[1:((dim(ace)[1])-5),c("axis1")])
-
-
-
-  sdf5<-apply(ace[,c("axis1","axis1.1","axis1.2","axis1.3","axis1.4","axis1.5")],1,sd)
-  meanf5<-apply(ace[,c("axis1","axis1.1","axis1.2","axis1.3","axis1.4","axis1.5")],1,mean)
-  ace$cvf5<-as.numeric(sdf5/meanf5)
-
-
-  sdf4<-apply(ace[,c("axis1._1","axis1","axis1.1","axis1.2","axis1.3","axis1.4")],1,sd)
-  meanf4<-apply(ace[,c("axis1._1","axis1","axis1.1","axis1.2","axis1.3","axis1.4")],1,mean)
-  ace$cvf4<-as.numeric(sdf4/meanf4)
-
-
-  sdf3<-apply(ace[,c("axis1._2","axis1._1","axis1","axis1.1","axis1.2","axis1.3")],1,sd)
-  meanf3<-apply(ace[,c("axis1._2","axis1._1","axis1","axis1.1","axis1.2","axis1.3")],1,mean)
-  ace$cvf3<-as.numeric(sdf3/meanf3)
-
-
-  sdf2<-apply(ace[,c("axis1._3","axis1._2","axis1._1","axis1","axis1.1","axis1.2")],1,sd)
-  meanf2<-apply(ace[,c("axis1._3","axis1._2","axis1._1","axis1","axis1.1","axis1.2")],1,mean)
-  ace$cvf2<-as.numeric(sdf2/meanf2)
-
-
-  sdf1<-apply(ace[,c("axis1._4","axis1._3","axis1._2","axis1._1","axis1","axis1.1")],1,sd)
-  meanf1<-apply(ace[,c("axis1._4","axis1._3","axis1._2","axis1._1","axis1","axis1.1")],1,mean)
-  ace$cvf1<-as.numeric(sdf1/meanf1)
-
-
-  sdb5<-apply(ace[,c("axis1._5","axis1._4","axis1._3","axis1._2","axis1._1","axis1")],1,sd)
-  meanb5<-apply(ace[,c("axis1._5","axis1._4","axis1._3","axis1._2","axis1._1","axis1")],1,mean)
-  ace$cvb5<-as.numeric(sdb5/meanb5)
-
-  ace$CV <- apply(ace[,c("cvb5","cvf1","cvf2","cvf3","cvf4","cvf5")],1,min)
-  ace$CV[is.na(ace$CV)]<-0
-
-
-  ace$mets[ace$axis1<=8] <- 1
-  ace$mets[ace$axis1>8 & ace$CV>10] <- 0.749395+(0.716431*(log(ace$axis1[ace$axis1>8 & ace$CV>10])))
-  -(0.179874*(log(ace$axis1[ace$axis1>8 & ace$CV>10]))^2)+
-    (0.033173*(log(ace$axis1[ace$axis1>8 & ace$CV>10]))^3)
-  ace$mets[ace$axis1>8 & ace$CV<=10] <- 2.294275*(exp(0.00084679*ace$axis1[ace$axis1>8 & ace$CV<=10]))
-
-
-  acti<-ace
-  rm(ace)
-  names(acti)[1]<-"date"
-  acti$date2 <- as.POSIXct(format(acti$date,format="%Y-%m-%d %H:%M"))
-  #acti2<-aggregate(subset(acti,select=c("mets")),by=list(acti$date2),FUN=mean)
-  acti2<-aggregate(mets~date2,data=acti,FUN=mean,na.rm=T)
-  names(acti2)<-c("date","mets.acti")
-  acti3<-aggregate(cbind(axis1,steps)~date2,data=acti,FUN=sum,na.rm=T)
-  names(acti3)<-c("date","axis1","steps")
-  acti4<-merge(acti3,acti2,by="date")
-
-  grup <- rle(acti4$axis1)
-
-  acti4$date.ch <- as.character(acti4$date)
-  acti4$wear <- PhysicalActivity::wearingMarking(acti4,TS="date.ch",cts="axis1",perMinuteCts=1)$wearing
-
-  acti4$wearing<-acti4$mets.acti
-  acti4$wearing[acti4$wear=="nw"]<-NA
-
-  acti4$day <- format(acti4$date,format="%Y-%m-%d")
-  acti4$worn[!is.na(acti4$wearing)==T]<- "Wearing Device, hours"
-  acti4$time.pa[acti4$wearing>=1.5 & acti4$wearing<3 & !is.na(acti4$wearing)] <- "Light Physical Activity (1.5-3 METs), hours"
-  acti4$time.pa[acti4$wearing>=3 & acti4$wearing<6 & !is.na(acti4$wearing)] <- "Moderate Physical Activity (3-6 METs), hours"
-  acti4$time.pa[acti4$wearing>=6 & !is.na(acti4$wearing)] <- "Vigorous Physical Activity (>6 METs), hours"
-  acti4
-
-  #anado wearing & METs al raw.data
-  acti <- merge(acti,acti4[c("date","wearing")],by.x="date2",by.y="date",all.x=T)
-  acti <- dplyr::tbl_df(acti)
-
-
-  return(acti)
-}
-
 
--- README.md
+++ README.md
@@ -7,3 +7,59 @@ Installation
 library("devtools")
 install_github("masalmon/convertagd")
 ```
+
+Example
+=======
+
+For now there is only a function for reading a agd file and getting two tables out of it.
+
+``` r
+library("convertagd")
+file <- system.file("extdata", "dummyCHAI.agd", package = "convertagd")
+testRes <- read_agd(file, tz = "GMT")
+kable(testRes[["settings"]])
+```
+
+|  settingID| settingName             | settingValue                                 |
+|----------:|:------------------------|:---------------------------------------------|
+|          1| softwarename            | ActiLife                                     |
+|          2| softwareversion         | 5.10.0                                       |
+|          3| osversion               | Microsoft Windows NT 6.1.7601 Service Pack 1 |
+|          4| machinename             | THINKCENTRE                                  |
+|          5| datetimeformat          | dd-MM-yyyy                                   |
+|          6| decimal                 | .                                            |
+|          7| grouping                | ,                                            |
+|          8| culturename             | English (India)                              |
+|          9| finished                | true                                         |
+|         10| devicename              | GT3XPlus                                     |
+|         11| filter                  | Normal                                       |
+|         12| deviceserial            | NEO1D31110533                                |
+|         13| deviceversion           | 2.5.0                                        |
+|         14| modenumber              | 61                                           |
+|         15| epochlength             | 10                                           |
+|         16| startdatetime           | 635670972000000000                           |
+|         17| stopdatetime            | 635672088000000000                           |
+|         18| downloaddatetime        | 635672179164562010                           |
+|         19| batteryvoltage          | 4.22                                         |
+|         20| original sample rate    | 30                                           |
+|         21| subjectname             | 11                                           |
+|         22| sex                     | Male                                         |
+|         23| race                    | Asian / Pacific Islander                     |
+|         24| limb                    | Other                                        |
+|         25| epochcount              | 11159                                        |
+|         26| sleepscorealgorithmname | Sadeh                                        |
+|         27| customsleepparameters   |                                              |
+|         28| notes                   |                                              |
+
+``` r
+kable(head(testRes[["raw.data"]]))
+```
+
+| date                |  axis1|  axis2|  axis3|  steps|  lux|  incline|
+|:--------------------|------:|------:|------:|------:|----:|--------:|
+| 2015-05-13 07:00:00 |      7|      1|     64|      1|    0|        3|
+| 2015-05-13 07:00:10 |      5|      0|     44|      0|    0|        3|
+| 2015-05-13 07:00:20 |      0|      0|     17|      0|    0|        3|
+| 2015-05-13 07:00:30 |      0|      0|      0|      0|    0|        3|
+| 2015-05-13 07:00:40 |      0|      0|      6|      0|    0|        3|
+| 2015-05-13 07:00:50 |      0|      0|      1|      0|    0|        3|
