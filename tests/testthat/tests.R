d71a475f83f6f30170bda28f2f7ac6275cbb1b34
masalmon <maelle.salmon@yahoo.se>
2016-01-27 13:37:39 +0100

    adds tests and codecov

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -19,3 +19,38 @@ if (requireNamespace("lintr", quietly = TRUE)) {
     lintr::expect_lint_free()
   })
 }
+
+#################################################################################################
+context("batch_read_agd")
+#################################################################################################
+test_that("batch_read_agd outputs files",{
+  skip_on_cran()
+  path_to_directory <- system.file("extdata", package = "convertagd")
+  batch_read_agd(path_to_directory, tz="GMT",
+                 all_in_one=TRUE)
+  expect_true(file.exists("settings.csv"))
+  expect_true(file.exists("raw_data.csv"))
+  batch_read_agd(path_to_directory, tz="GMT",
+                 all_in_one=FALSE)
+  expect_true(file.exists("dummyCHAI2_settings.csv"))
+  expect_true(file.exists("dummyCHAI2_raw.csv"))
+  expect_true(file.exists("dummyCHAI_settings.csv"))
+  expect_true(file.exists("dummyCHAI_raw.csv"))
+
+})
+
+test_that("batch_read_agd outputs errors",{
+  skip_on_cran()
+  path_to_directory <- system.file("extdata", package = "convertagd")
+  expect_error(batch_read_agd(path_to_directory, tz="GMT",
+                 all_in_one=TRUE),"There are already a settings.csv and/or a raw_data.csv in your working directory !")
+  expect_error(batch_read_agd(path_to_directory, tz="GMT",
+                              all_in_one=FALSE),"There are already adummyCHAI_settings.csv and/or adummyCHAI_raw.csv in your working directory !")
+
+  file.remove("settings.csv")
+  file.remove("raw_data.csv")
+  file.remove("dummyCHAI2_settings.csv")
+  file.remove("dummyCHAI2_raw.csv")
+  file.remove("dummyCHAI_settings.csv")
+  file.remove("dummyCHAI_raw.csv")
+})
